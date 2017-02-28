1fff160ac325c2d9017cce251ff93a576e19c4c5
Gala <Gala>
2017-02-28 05:03:08 +0200

    lined js

--- js/script.js
+++ js/script.js
@@ -12,53 +12,53 @@ var go = 0;
 
 function startClick() {
 
-	btnStart.innerHTML = 'Pause';
+  btnStart.innerHTML = 'Pause';
 
-	if (go == 0) {
+  if (go == 0) {
 
-		timerId = setInterval(function() {
-			count += 10;
+    timerId = setInterval(function() {
+      count += 10;
 
       var hour = Math.floor(count/3600000)%24;
-				if (hour < 10) {
+        if (hour < 10) {
           hour = '0' + hour;
         };
-			var min = Math.floor(count/60000)%60;
-				if (min < 10) {
+      var min = Math.floor(count/60000)%60;
+        if (min < 10) {
           min = '0' + min;
         };
-			var sec = Math.floor(count/1000)%60;
-				if (sec < 10) {
+      var sec = Math.floor(count/1000)%60;
+        if (sec < 10) {
           sec = '0' + sec;
         };
-			var mSek = count % 1000;
-				if (mSek < 100) {
+      var mSek = count % 1000;
+        if (mSek < 100) {
           mSek = '0' + mSek;
         };
-				if (mSek < 10) {
+        if (mSek < 10) {
           mSek = '0' + mSek;
         };
 
-			btnTimer.innerHTML = hour + ' : ' + min + ' : ' + sec;
+      btnTimer.innerHTML = hour + ' : ' + min + ' : ' + sec;
       forMsek.innerHTML = ' ' + mSek;
 
-		}, 10);
+    }, 10);
 
-		go = 1;
+    go = 1;
 
-	} else {
+  } else {
 
-		clearInterval(timerId);
-		go = 0;
-		btnStart.innerHTML = 'Start';
-	}
-}
+    clearInterval(timerId);
+    go = 0;
+    btnStart.innerHTML = 'Start';
+  };
+};
 
 function resetClick() {
-	clearInterval(timerId);
-	go = 0;
-	count = 0;
-	btnTimer.innerHTML = '00 : 00 : 00';
+  clearInterval(timerId);
+  go = 0;
+  count = 0;
+  btnTimer.innerHTML = '00 : 00 : 00';
   forMsek.innerHTML = ' 000';
-	btnStart.innerHTML = 'Start';
-}
+  btnStart.innerHTML = 'Start';
+};
